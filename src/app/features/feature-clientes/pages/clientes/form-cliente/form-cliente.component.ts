8bfed33745be99e0cacb4c6246f4ffdca717c70c
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2022-06-03 11:04:50 -0500

    sin debuger

--- src/app/features/feature-clientes/pages/clientes/form-cliente/form-cliente.component.ts
+++ src/app/features/feature-clientes/pages/clientes/form-cliente/form-cliente.component.ts
@@ -43,7 +43,6 @@ export class FormClienteComponent implements OnInit {
   else{
     console.table(cliente); 
     if (this.formGroup.valid) {
-      debugger
       this.clienteService.UpdateCliente(cliente.cedula,cliente).subscribe(t=>{
         var result = t
         this.mensaje.mensajeAlertaCorrecto("Cliente Actualizado Correctamente");
